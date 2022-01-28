5eeb7f3121e83897fd05aad14522f3c98a0d43a6
Sergio Vargas <sergioperalta04@hotmail.com>
2022-01-27 22:00:03 -0500

    Agregamos los extras

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,8 +2,13 @@ def run():
     nombre=input("Escribe tu nombre: ")
     edad=int(input("Escribe tu edad: "))
     fecha100=2022-edad+100
-    print(nombre+ " En el año "+str(fecha100)+" tendrás 100 años")
-
+    def imprimir_mensaje():
+        print(nombre+ """ en el año """+str(fecha100)+""" tendrás 100 años        
+        """)
+    imprimir_mensaje()
+    repeticiones=int(input("Escribe un numero: "))
+    for x in range(repeticiones):
+        imprimir_mensaje()
 
 if __name__=="__main__":
     run()
