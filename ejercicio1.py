013789197713619fc9b69d2edfe691129fc4a9fa
Sergio Vargas <sergioperalta04@hotmail.com>
2022-01-27 20:40:24 -0500

    Solucion primer ejercicios

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+def run():
+    nombre=input("Escribe tu nombre: ")
+    edad=int(input("Escribe tu edad: "))
+    fecha100=2022-edad+100
+    print(nombre+ " En el año "+str(fecha100)+" tendrás 100 años")
+
+
+if __name__=="__main__":
+    run()
